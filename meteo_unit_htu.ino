c870097826febf3ff396d069fe1483882fafaa80
Jakub Štajner <kubik256@gmail.com>
2017-05-10 15:58:48 +0200

    Add files via upload
    This is INO file for small indoor units of ESP8266 together with HTU21D sensor for messuring indoor temperature and humidity.
    (Smaller version of meteo main INO, using the same system, only chnange is number of sensors and sending MQTT values as JSON payload as unit + UNIT_NO to make many of those numbered units)

---
+++ meteo_unit_htu.ino
@@ -0,0 +1,63 @@
+/********************************************************
+ESP8266 (ESP-12E) IoT weather module unit
+---------------------------------------------------------
+(0x76) BME280  - temp,humi,pressure
+(0x40) HTU21D  - temp,humi
+********************************************************/
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ArduinoJson.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <PubSubClient.h>
+#include <SparkFunHTU21D.h>
+/******************** ADC MACRO FOR VCC MESSUREMENT ******/
+ADC_MODE(ADC_VCC);
+/******************** DEFUALT CONFIGURATION **************/
+const int UNIT_NO = 1; /* YOU CAN BUILD AS MANY UNITS AS YOU WANT, BUT ALWAYS INCRASE UNIT_NO */
+const char* MQTT_SERVER = "############ MQTT address goes here ############";
+const char* MQTT_USER = "############ MQTT name goes here ############";
+const char* MQTT_PASS = "############ MQTT password goes here ############";
+const int MQTT_PORT = 1883;
+const int ESP_SLEEP = 300; /* SLEEP TIME (s) */
+/******************** MAIN VARIABLES *********************/
+WiFiClient espClient;
+PubSubClient client(MQTT_SERVER,MQTT_PORT,espClient);
+HTU21D htu;
+float t,h,b;
+char buff [12]; 
+/******************** SETUP ON BOOT **********************/
+void setup(){
+  //Serial.begin(9600);
+  /* WIFI MANAGER PART */
+  WiFiManager wifiManager;
+  if(!wifiManager.autoConnect()){
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  }
+  /* START SENSORS */
+  htu.begin();
+  /* START MQTT CLIENT */
+  sprintf(buff, "unit%d", UNIT_NO);
+  client.connect(buff,MQTT_USER,MQTT_PASS);
+  delay(100);
+}
+/******************** MAIN LOOP **************************/
+void loop(){
+  /* HTU21D */
+  h = htu.readHumidity();
+  t = htu.readTemperature();
+  /* VOLTAGE */
+  b = ESP.getVcc() / 1000.0;
+  /* PUBLISH ALL VALUES AS JSON PAYLOAD VIA MQTT */
+  char buff2 [256];
+  DynamicJsonBuffer jsonBuffer;
+  sprintf(buff, "iot/unit%d", UNIT_NO);
+  JsonObject& root = jsonBuffer.createObject();
+  root["t"] = t; root["h"] = h; root["b"] = b;
+  root.printTo(buff2, sizeof(buff2));
+  client.publish(buff,buff2,true);
+  /* DEEP SLEEEP */
+  ESP.deepSleep(ESP_SLEEP * 1000000);
+}
